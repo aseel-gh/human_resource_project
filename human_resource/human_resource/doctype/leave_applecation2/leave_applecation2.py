6d4b6a76582c4d7c4c5ffdc6a659b56cdc7b46c1
aseel-gh <aseel.gharbia@gmail.com>
2023-01-28 16:36:26 +0200

    Merge pull request #19792 from barredterra/responsive-freeze-message
    feat: Initialize App

---
+++ human_resource/human_resource/doctype/leave_applecation2/leave_applecation2.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2023, aseel-gh and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.utils import date_diff
+from frappe.model.document import Document
+
+
+class LeaveApplecation2(Document):
+	pass
