0f30c09cf2cba7afc5b89d46f1205d11eeb618bb
aseel-gh <aseel.gharbia@gmail.com>
2023-01-30 15:58:49 +0200

    update1

--- human_resource/human_resource/doctype/my__leave_allocation/test_my__leave_allocation.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, aseel-gh and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestMyLeaveAllocation(FrappeTestCase):
-	pass
